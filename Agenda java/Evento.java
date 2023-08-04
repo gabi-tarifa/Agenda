af2c20ffb89fa7c792c5f219b6654913c93f5ef9
MeowGamer <126773354+gabi-tarifa@users.noreply.github.com>
2023-08-04 08:14:36 -0300

    Add files via upload

---
+++ Agenda java/Evento.java
@@ -0,0 +1,7 @@
+public class Evento extends Agenda{
+    private int dia0, mes0, ano0, hora0, minuto0;
+    private int[] data0 = new int[5];
+    private int dia, mes, ano, hora, minuto;
+    private int[] data = new int[5];
+    private String nome; 
+}
