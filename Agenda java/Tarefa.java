f9bcee322e96522531bb4582f24435a918f25ac6
MeowGamer <126773354+gabi-tarifa@users.noreply.github.com>
2023-08-04 08:15:59 -0300

    Delete Agenda java directory

--- Agenda java/Tarefa.java
+++
@@ -1,7 +0,0 @@
-public class Tarefa extends Agenda {
-    private int dia0, mes0, ano0, hora0, minuto0;
-    private int[] data0 = new int[5];
-    private int dia, mes, ano, hora, minuto;
-    private int[] data = new int[5];
-    private String nome;    
-}
